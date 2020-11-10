99128936500495f6c9a9031e0593c7776f64a085
Jozef Plata <plata.jozef@example.com>
2020-11-10 12:52:24 +0100

    Implemented PlayerMovementController

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed = 6.5f;
+    [SerializeField] private float _jumpForce = 500;
+    private Rigidbody2D _rigidbody2D;
+
+    private void Awake()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float _horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 _movement = new Vector3(_horizontal, 0);
+
+        MovePlayer(_movement);
+    }
+
+    private void MovePlayer(Vector3 movement)
+    {
+        transform.position += movement * Time.deltaTime * _moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _jumpForce);
+        }
     }
 }
