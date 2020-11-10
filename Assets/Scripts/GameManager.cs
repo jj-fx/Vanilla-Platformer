6d0e1352c54d930be3c0874454f103039e477c68
Jozef Plata <plata.jozef@example.com>
2020-11-10 14:56:47 +0100

    KillOnTouch - kills the player
    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _maxLives = 3;
+
+    public int Lives { get; private set; }
+    public GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            Lives = _maxLives;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    internal static void KillPlayer()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var playerMovementController = collision.collider.GetComponent<PlayerMovementController>();
+        if (playerMovementController != null)
+        {
+            GameManager.KillPlayer();
+        }
+    }
+}
