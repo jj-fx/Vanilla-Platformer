e6f18e8949769310d29930fc72f1a319ab350760
Jozef Plata <plata.jozef@example.com>
2020-11-11 13:30:10 +0100

    CharacterGrounding now parenting to moving objects.
    Using 3 contact points to determine the parent. I might want to change it to 2 later

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -1,19 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterGrounding : MonoBehaviour
 {
-    [Tooltip("Use Two Feet")]
-    [SerializeField] private Transform[] _feet = new Transform[2];
+    [Tooltip("Use Three Feet")]
+    [SerializeField] private Transform[] _feet = new Transform[3];
     [SerializeField] private float _maxDistance = 0.25f;
     [SerializeField] private LayerMask _layerMask;
+    private Transform[] _groundedObject = new Transform[3];
 
     public bool IsGrounded { get; private set; }
 
+
     private void Update()
     {
-        if (IsFootGrounded(_feet[0]) || IsFootGrounded(_feet[1]))
+        var leftFoot = IsFootGrounded(0);
+        var middleFoot = IsFootGrounded(1);
+        var rightFoot = IsFootGrounded(2);
+
+        if (leftFoot || middleFoot || rightFoot)
         {
             IsGrounded = true;
         }
@@ -21,18 +25,66 @@ public class CharacterGrounding : MonoBehaviour
         {
             IsGrounded = false;
         }
+
+        StickToMovingObjects();
     }
 
-    private bool IsFootGrounded(Transform foot)
+    private void StickToMovingObjects()
     {
-        var _raycatHit = Physics2D.Raycast(foot.position, Vector2.down, _maxDistance, _layerMask);
+        Transform groundedObject = GetGroundedObject();
+        
+        if (groundedObject != null)
+        {
+            transform.parent = groundedObject;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+    }
+
+    private Transform GetGroundedObject()
+    {
+        Transform groundedObject;
+
+        if (_groundedObject[0] == _groundedObject[1])
+        {
+            groundedObject = _groundedObject[0];
+        }
+        else if (_groundedObject[2] == _groundedObject[1])
+        {
+            groundedObject = _groundedObject[2];
+        }
+        else
+        {
+            groundedObject = null;
+        }
+
+        return groundedObject;
+    }
+
+    private bool IsFootGrounded(int foot)
+    {
+        var _raycatHit = Physics2D.Raycast(_feet[foot].position, Vector2.down, _maxDistance, _layerMask);
 
         if (_raycatHit.collider != null)
         {
+            var collider = _raycatHit.collider;
+
+            if (collider.GetComponent<Coin>() == null)
+            {
+                _groundedObject[foot] = collider.transform;
+            }
+            else
+            {
+                _groundedObject[foot] = null;
+            }
+
             return true;
         }
         else
         {
+            _groundedObject[foot] = null;
             return false;
         }
     }
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform _start;
+    [SerializeField] private Transform _end;
+    [SerializeField] private float _speed = 4;
+    private SpriteRenderer _sprite;
+    private float _positionPercent = 0;
+    private float _direction = 1;
+
+    private Vector3 _startPosition;
+    private Vector3 _endPosition;
+
+    private void Awake()
+    {
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+
+        _endPosition = _end.position;
+        _startPosition = _start.position;
+
+        AdjustPositionsByColliderType();
+    }
+
+    private void Update()
+    {
+        var distance = Vector3.Distance(_startPosition, _endPosition);
+        var speedByDistance = _speed / distance;
+
+        _positionPercent += Time.deltaTime * _direction * speedByDistance;
+
+        _sprite.transform.position = Vector3.Lerp(_startPosition, _endPosition, _positionPercent);
+
+        if (_positionPercent >= 1 && _direction == 1)
+        {
+            _direction = -1;
+        }
+        else if (_positionPercent <= 0 && _direction == -1)
+        {
+            _direction = 1;
+        }
+    }
+
+    private void AdjustPositionsByColliderType()
+    {
+        bool circleTypeCollider = false;
+
+        if (GetComponentInChildren<CircleCollider2D>() == null)
+        {
+            circleTypeCollider = false;
+        }
+        else if (GetComponentInChildren<BoxCollider2D>() == null)
+        {
+            circleTypeCollider = true;
+        }
+
+        if (!circleTypeCollider)
+        {
+            var radius = GetComponentInChildren<BoxCollider2D>().bounds.size.x;
+            _endPosition.x -= radius * 0.5f;
+            _startPosition.x += radius * 0.5f;
+        }
+        else
+        {
+            var radius = GetComponentInChildren<CircleCollider2D>().bounds.size.x;
+            _endPosition.x -= radius * 0.5f;
+            _startPosition.x += radius * 0.5f;
+        }
+    }
+}
