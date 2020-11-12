115e696ec79e9a6458144d92b2a7db32dd9c9b75
Jozef Plata <plata.jozef@example.com>
2020-11-12 16:55:08 +0100

    Added background animation and Main Menu hide animations

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = _camera.transform.position * 0.15f;
+    }
+}
--- Assets/Scripts/UI/UIPlayButton.cs
+++ Assets/Scripts/UI/UIPlayButton.cs
@@ -1,9 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class UIPlayButton : MonoBehaviour
 {
+    [SerializeField] private GameObject _settingsButton;
+    [SerializeField] private GameObject _background;
+    [SerializeField] private GameObject _title;
+
     public void StartGame()
     {
+        StartCoroutine(WaitToLoadLevel(1f));
+    }
+
+    private IEnumerator WaitToLoadLevel(float time)
+    {
+        GetComponent<Animator>().SetTrigger("Play");
+        _settingsButton.GetComponent<Animator>().SetTrigger("Play");
+        _background.GetComponent<Animator>().SetTrigger("Hide");
+        _title.GetComponent<Animator>().SetTrigger("Hide");
+
+        yield return new WaitForSeconds(time);
+
         GameManager.Instance.GoToNextLevel();
     }
 }
